7e2d0dd5faa48c47a213b6ee3e7709e558afec89
Rachit Verma <rachit.verma@protonshub.in>
2021-12-22 17:39:07 +0530

    serializer fixes

--- app/serializers/api/v1/companies_serializer.rb
+++ app/serializers/api/v1/companies_serializer.rb
@@ -4,7 +4,7 @@ class Api::V1::CompaniesSerializer < ActiveModel::Serializer
   attributes :id, :name, :posts, :foundation_year, :tagline, :city, :followers, :no_of_employees, :logo, :url, :company_type, :resync_progress
 
   def posts
-    object.posts&.join(", ")&.downcase
+    object.posts.class == Array ? object.posts&.join(", ")&.downcase : object.posts
   end
 
 end
